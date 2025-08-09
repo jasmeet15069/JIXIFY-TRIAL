ee07a205e51ba541811776018ac1ff8792dd5675
jasmeet15069 <jasmeetsayss@gmail.com>
2025-08-10 01:15:13 +0530

    V1.0.2

--- api/index.js
+++ api/index.js
@@ -153,4 +153,6 @@ app.post("/chat", authenticateToken, async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// app.listen(3000, () => console.log("Server running on port 3000"));
+
+module.exports = app;
